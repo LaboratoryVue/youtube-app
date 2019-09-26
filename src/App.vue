9948dc68c5fde921e755e9d037428b4f342b1827
Moe Green <gearmobile@gmail.com>
2019-09-26 21:19:46 +0300

    feat: add two layouts

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link></div>
-    <router-view />
+    <Navigation></Navigation>
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation";
+import EmptyLayout from "./layouts/EmptyLayout";
+import MainLayout from "./layouts/MainLayout";
+
+export default {
+  name: "app",
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || "empty") + "-layout";
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout,
+    Navigation
+  }
+};
+</script>
 
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link to="/">home</router-link>
+    <router-link to="/login">login</router-link>
+    <router-link to="/categories">categories</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+};
+</script>
+
+<style scoped></style>
