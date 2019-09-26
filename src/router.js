6b4bba5484bf0471a643dd8c8aaae5562fe237ad
Moe Green <gearmobile@gmail.com>
2019-09-26 21:49:16 +0300

    feat: add rest views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -11,7 +10,8 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      meta: { layout: "main" },
+      component: () => import("./views/Home")
     },
     {
       path: "/login",
@@ -24,6 +24,36 @@ export default new Router({
       name: "categories",
       meta: { layout: "main" },
       component: () => import("./views/Categories")
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      meta: { layout: "main" },
+      component: () => import("./views/Profile")
+    },
+    {
+      path: "/record",
+      name: "record",
+      meta: { layout: "main" },
+      component: () => import("./views/Record")
+    },
+    {
+      path: "/detail-record",
+      name: "detail-record",
+      meta: { layout: "main" },
+      component: () => import("./views/Detail-Record")
+    },
+    {
+      path: "/history",
+      name: "history",
+      meta: { layout: "main" },
+      component: () => import("./views/History")
+    },
+    {
+      path: "/planning",
+      name: "planning",
+      meta: { layout: "main" },
+      component: () => import("./views/Planning")
     }
   ]
 });
